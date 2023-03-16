d1144bcb99d4d7f86d3301fabbb106a47dc70e76
linocsar <linocsar.dev@gmail.com>
2023-03-16 12:13:16 -0500

    Fin de la seccion 4 creacion de modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//Modulos
+import { PagesRoutingModule } from './pages/pages.routing';
 
-const routes: Routes = [];
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { AuthRoutingModule } from './auth/auth.routing';
+
+const routes: Routes = [
+  { path: '', redirectTo:'/dashboard', pathMatch:'full'},
+  { path: '**', component: NopagefoundComponent}
+
+  //path: '/dashboard' PagesRouting
+  //path: '/auth' AuthRouting
+  //path: '/medicos' MedicosRouting
+  //path: '/compras' ComprasRouting
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
+
 import { AppComponent } from './app.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NopagefoundComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    PagesModule,
+    AppRoutingModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
